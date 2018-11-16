a0488edd602d78170a0cd1ece0ed5305ab33f866
Dirk Schut <dschut@outlook.com>
2018-11-16 11:34:00 +0100

    Multiple resources implemented

--- behavior.js
+++ behavior.js
@@ -2,6 +2,12 @@ $( document ).ready(function(){
     console.log("Welcome");
 
     window.counter = 0;
+    window.resources = [];
+    window.resources["logs"] = 0;
+    window.resources["planks"] = 0;
+    window.resources["stone"] = 0;
+    window.resources["food"] = 0;
+    setUserResource("logs");
 
     window.setInterval(tick, 1000);
     window.setInterval(frame, 10);
@@ -12,12 +18,43 @@ $( document ).ready(function(){
 });
 
 function tick(){
-    window.counter++;
+    window.resources[window.userResource]++;
 }
 
 function frame(){
-    $("#LogsAmount").text(window.counter);
-    $("#PlanksAmount").text(window.counter);
-    $("#StoneAmount").text(window.counter);
-    $("#FoodAmount").text(window.counter);
+    $("#LogsAmount").text(window.resources["logs"]);
+    $("#PlanksAmount").text(window.resources["planks"]);
+    $("#StoneAmount").text(window.resources["stone"]);
+    $("#FoodAmount").text(window.resources["food"]);
+}
+
+function setUserResource(resource){
+    console.log(resource);
+    clearManualResource();
+    window.userResource = resource;
+    switch(resource){
+        case "logs":
+            $("#LogsUserButton").addClass("userDoing");
+            break;
+        case "planks":
+            $("#PlanksUserButton").addClass("userDoing");
+            break;
+        case "stone":
+            $("#StoneUserButton").addClass("userDoing");
+            break;
+        case "food":
+            $("#FoodUserButton").addClass("userDoing");
+            break;
+        default:
+            window.userResource = "logs";
+            console.log("UNKNOWN USER RESOURCE: " + resource);
+            break;
+    }
+}
+
+function clearManualResource(){
+    $("#LogsUserButton").removeClass("userDoing");
+    $("#PlanksUserButton").removeClass("userDoing");
+    $("#StoneUserButton").removeClass("userDoing");
+    $("#FoodUserButton").removeClass("userDoing");
 }
